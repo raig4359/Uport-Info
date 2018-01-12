15daaf11b3dbb6da91d346634b43c6926115e95f
Gaurav <gaurav@ediffy.com>
2018-01-12 18:54:33 +0530

    second commit

--- app/src/main/java/com/raig/uportinfo/splash/SplashActivity.java
+++ app/src/main/java/com/raig/uportinfo/splash/SplashActivity.java
@@ -1,12 +1,23 @@
 package com.raig.uportinfo.splash;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.raig.uportinfo.login.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        },1200);
     }
 }
