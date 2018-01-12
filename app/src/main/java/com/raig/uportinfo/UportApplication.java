01e6e8b48c429c33bb31b2e9f8ccd11ea3b791c1
Gaurav <gaurav@ediffy.com>
2018-01-12 12:51:04 +0530

    first commit

---
+++ app/src/main/java/com/raig/uportinfo/UportApplication.java
@@ -0,0 +1,16 @@
+package com.raig.uportinfo;
+
+import android.app.Application;
+
+/**
+ * Created by Gaurav on 12-01-2018.
+ */
+
+public class UportApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
